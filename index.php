e1d53abc925a70fddfdefc4346618e7aa6b4472b
sherred <paolo.ciarrocca@gmail.com>
2014-12-17 11:19:56 +0000

    Make style guide more mobile first, css/sass file includes and structure, markup cleanup. add .gitignore

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en" class="guide">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pattern Primer</title>
     <link rel="stylesheet" href="global.css">
     <link rel="stylesheet" href="guide.css">
@@ -15,36 +17,24 @@
   $files = glob("patterns/*.html");
   sort($files);
   foreach ($files as $file):
-      echo '<div class="pattern">';
-      echo '<div class="display">';
-      include($file);
-      echo '</div>';
-      echo '<div class="source">';
-      echo '<h2>Markup</h2>';
-      echo '<pre>';
-      echo '<code>';
-      echo htmlspecialchars(file_get_contents($file));
-      echo '</code>';
-      echo '</pre>';
-      echo '</div>';
-      echo '<div class="style">';
-      echo '<h2>CSS/Sass</h2>';
-      echo '<pre>';
-      echo '<code class="sass">';
-      echo '.homeblock a {
-  color: darken($navcolor,5%);
-  text-decoration: none;
-  &:hover {
-    color: $navcolor;
-    text-decoration: underline;
-  }
-}';
-      echo '</code>';
-      echo '</pre>';
-      echo '</div>';
-      echo '</div>';
-  endforeach;
   ?>
-  
+    <div class="pattern">
+      <div class="display">
+      <?php include($file); ?>
+      </div>
+      <div class="source">
+        <h2>Markup</h2>
+        <pre>
+          <code><?php echo htmlspecialchars(file_get_contents($file)); ?></code>
+        </pre>
+      </div>
+      <div class="style">
+        <h2>CSS/Sass</h2>
+        <pre>
+          <code><?php echo file_get_contents("css/sass/_".basename($file, ".html").".scss"); ?></code>
+        </pre>
+      </div>
+    </div>
+  <?php endforeach; ?>
   </body>
 </html>

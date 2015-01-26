e3bde49497d3b6917a5c6cbca3119f769049ce95
Ben Seven <hello@benseven.co.uk>
2015-01-26 17:48:23 +0000

    Trying something out to display git tag instead of branch

--- functions.php
+++ functions.php
@@ -1,9 +1,22 @@
-<?php // Get the current branch from Git
-        $stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
+<?php 
+  // Get the current branch from Git
 
-        $firstLine = $stringfromfile[0]; //get the string from the array
-    
-        $explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
-    
-        $branchname = $explodedstring[2]; //get the one that is always the branch name
+  /*  $stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
+
+    $firstLine = $stringfromfile[0]; //get the string from the array
+
+    $explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
+
+    $branchname = $explodedstring[2]; //get the one that is always the branch name */
+?>
+
+<?php 
+  
+  $stringfromfile = file('.git/packed-refs', FILE_USE_INCLUDE_PATH);
+
+  echo $stringfromfile[2];
+
+
+  $branchname = foo;
+
 ?>

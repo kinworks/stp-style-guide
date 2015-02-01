d07486f3b238e3948d6905027847d6c42d8a0f9d
Ben Seven <hello@benseven.co.uk>
2015-02-01 22:42:35 +0000

    Deploymonkey addition

--- deploymonkey.php
+++ deploymonkey.php
@@ -1,18 +1,1 @@
-<?php
-	`$ProjectName = STP Style Guide;
-
-	wall DeployMonkey running for $ProjectName;
-	git pull --no-edit origin master;
-
-	# Get new tags from remote
-	git fetch --tags;
-
-	# Get latest tag name
-	latestTag=$(git describe --tags `git rev-list --tags --max-count=1`);
-
-	# Checkout latest tag
-	git checkout $latestTag;
-
-	wall DeployMonkey successfully deployed $ProjectName $latestTag;
-
-`; ?>
+<?php `sh deploy.sh 2>&1`; ?>
